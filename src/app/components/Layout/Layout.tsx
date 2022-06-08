340b359db1e6bd6ce09ce708eae4e489182b71d6
Adrian Velasco <adrivelasco.dev@gmail.com>
2022-06-07 23:05:04 -0300

    feat: add footer

--- src/app/components/Layout/Layout.tsx
+++ src/app/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { AddIcon } from '@chakra-ui/icons';
-import { Box, BoxProps, Button } from '@chakra-ui/react';
+import { Box, BoxProps, Button, Text } from '@chakra-ui/react';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useCallback } from 'react';
 
@@ -18,18 +18,27 @@ export const Layout = (props: LayoutProps) => {
     <Box maxWidth={1280} mx="auto" position="relative" {...props}>
       <Box
         alignItems="center"
+        as="header"
         display="flex"
         justifyContent="space-between"
         px={4}
         py={6}
       >
         <PokedexLogo onClick={handleOnClickLogo} />
-        <Button variant="outline" colorScheme="cyan" leftIcon={<AddIcon />}>
+        <Button variant="solid" colorScheme="red" leftIcon={<AddIcon />}>
           Add Pokemon
         </Button>
       </Box>
+
       <Box as="main" position="relative">
         <Outlet />
+      </Box>
+
+      <Box as="footer" px={4} textAlign="right">
+        <Box height={1} bg="gray.200" my={5} />
+        <Text fontSize={14} color="gray.600">
+          Adrian Velasco @ 2022
+        </Text>
       </Box>
     </Box>
   );
